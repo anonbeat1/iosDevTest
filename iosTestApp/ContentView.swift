0013e9e412c09936cc04c926feed4f5fdfe3914a
Gabriele Spilotri Cavaiola <gabriele.spilotri.cavaiola@atlantica.it>
2023-10-16 12:36:49 +0200

    Initial Commit

---
+++ iosTestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iosTestApp
+//
+//  Created by Gabriele Spilotri Cavaiola on 13/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
---
+++ iosTestApp/iosTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosTestAppApp.swift
+//  iosTestApp
+//
+//  Created by Gabriele Spilotri Cavaiola on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct iosTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
